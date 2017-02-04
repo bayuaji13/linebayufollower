1c10d673a776fc9c830e2873c563dd7458697a3d
bayuaji13 <bayu.xiii@gmail.com>
2017-02-05 05:20:04 +0700

    class sensor (kurang konversi 16 ke 50)

--- sensor.py
+++ sensor.py
@@ -1,64 +1,60 @@
 import RPi.GPIO as GPIO
 import time
 
-pin1 = 3;
-pin2 = 10;
-pin3 = 7;
-pin4 = 8;
+class Sensor:
 
-pink1 = 15;
-pink2 = 13;
-pink3 = 11;
-pink4 = 12;
-kiri = 0;
-kanan = 0;
-GPIO.setmode(GPIO.BOARD);
+    def __init__(self,pinLeft1,pinLeft2,pinLeft3,pinLeft4,pinRight1,pinRight2,pinRight3,pinRight4):
 
-GPIO.setup(pin1, GPIO.IN);
-GPIO.setup(pin2, GPIO.IN);
-GPIO.setup(pin3, GPIO.IN);
-GPIO.setup(pin4, GPIO.IN);
-GPIO.setup(pink1, GPIO.IN);
-GPIO.setup(pink2, GPIO.IN);
-GPIO.setup(pink3, GPIO.IN);
-GPIO.setup(pink4, GPIO.IN);
+        GPIO.setmode(GPIO.BCM)
 
-i=0;
-t_end = time.time() + 20;
-# while time.time() < t_end:
-while 1:
-	i = i+1;
-	if (not GPIO.input(pin1)) :
-		# print("1");
-		kiri = kiri + 8;
-	if (not GPIO.input(pin2)) :
-		# print("2");
-		kiri = kiri + 4;
-	if (not GPIO.input(pin3)) :
-		# print("3");
-		kiri = kiri + 2;
-	if (not GPIO.input(pin4)) :
-		# print("4");
-		kiri = kiri + 1;
-	# print(bin(kiri));
-	# kiri = 0;
-	if (not GPIO.input(pink1)) :
-		# print("5");
-		kanan = kanan + 8;
-	if (not GPIO.input(pink2)) :
-		# print("6");
-		kanan = kanan + 4;
-	if (not GPIO.input(pink3)) :
-		# print("7");
-		kanan = kanan + 2;
-	if (not GPIO.input(pink4)) :
-		# print("8");
-		kanan = kanan + 1;
-	# print(bin(kanan));
-	print "kiri = %d kanan = %d " % (kiri, kanan);
-	# print "kiri = %s kanan = %s " % (bin(kiri), bin(kanan));
-	kanan = 0;
-	kiri = 0;
-	# print(i);
-	time.sleep(0.5);
-print(i);
+		self.sensorLeft1 = pinLeft1;
+		self.sensorLeft2 = pinLeft2;
+		self.sensorLeft3 = pinLeft3;
+		self.sensorLeft4 = pinLeft4;
+
+		self.sensorRight1 = pinRight1;
+		self.sensorRight2 = pinRight2;
+		self.sensorRight3 = pinRight3;
+		self.sensorRight4 = pinRight4;
+
+        GPIO.setup(self.sensorLeft1,GPIO.IN);
+        GPIO.setup(self.sensorLeft2,GPIO.IN);
+        GPIO.setup(self.sensorLeft3,GPIO.IN);
+        GPIO.setup(self.sensorLeft4,GPIO.IN);
+
+        GPIO.setup(self.sensorRight1,GPIO.IN);
+        GPIO.setup(self.sensorRight2,GPIO.IN);
+        GPIO.setup(self.sensorRight3,GPIO.IN);
+        GPIO.setup(self.sensorRight4,GPIO.IN);
+	
+	def ReadSensor(self) :
+		right = 0;
+		left = 0;
+
+		if (not GPIO.input(self.sensorLeft1)) :
+			# print("1");
+			left = left + 8;
+		if (not GPIO.input(self.sensorLeft2)) :
+			# print("2");
+			left = left + 4;
+		if (not GPIO.input(self.sensorLeft3)) :
+			# print("3");
+			left = left + 2;
+		if (not GPIO.input(self.sensorLeft4)) :
+			# print("4");
+			left = left + 1;
+		# print(bin(kiri));
+		# kiri = 0;
+		if (not GPIO.input(self.sensorRight1)) :
+			# print("5");
+			right = right + 8;
+		if (not GPIO.input(self.sensorRight2)) :
+			# print("6");
+			right = right + 4;
+		if (not GPIO.input(self.sensorRight3)) :
+			# print("7");
+			right = right + 2;
+		if (not GPIO.input(self.sensorRight4)) :
+			# print("8");
+			right = right + 1;
+		return right - left;
